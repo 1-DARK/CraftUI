3cbf9ee0298b3698734b650bf6b98a4f26e4d8ed
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-02 13:36:27 +0530

    changes in navbar

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
+      <div className="container mx-auto px-4 py-3">
+        <div className="flex justify-between items-center">
+          {/* Logo */}
+          <Link
+            to="/"
+            className="text-2xl font-bold text-emerald-400 flex items-center space-x-2"
+          >
+            CraftUI
+          </Link>
+
+          {/* Navigation */}
+          <nav className="flex items-center gap-3 sm:gap-4">
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
+            >
+              <span
+                className="text-gray-300 hover:text-emerald-400 transition duration-300
+					 ease-in-ou"
+              >
+                Home
+              </span>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
