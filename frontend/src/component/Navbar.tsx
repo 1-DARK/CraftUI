1f8084f521b90132573c3029750f4d13f3e5acf1
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-02 13:42:02 +0530

    add pages in navbar component

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
-      <div className="container mx-auto px-4 py-3">
-        <div className="flex justify-between items-center">
+      <div className="container px-4 py-3">
+        <div className="flex gap-x-3 justify-items-start items-center">
           {/* Logo */}
           <Link
             to="/"
@@ -23,7 +23,79 @@ const Navbar = () => {
                 className="text-gray-300 hover:text-emerald-400 transition duration-300
 					 ease-in-ou"
               >
-                Home
+                Docs
+              </span>
+            </Link>
+
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
+            >
+              <span
+                className="text-gray-300 hover:text-emerald-400 transition duration-300
+					 ease-in-ou"
+              >
+                Components
+              </span>
+            </Link>
+
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
+            >
+              <span
+                className="text-gray-300 hover:text-emerald-400 transition duration-300
+					 ease-in-ou"
+              >
+                Blocks
+              </span>
+            </Link>
+
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
+            >
+              <span
+                className="text-gray-300 hover:text-emerald-400 transition duration-300
+					 ease-in-ou"
+              >
+                Charts
+              </span>
+            </Link>
+
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
+            >
+              <span
+                className="text-gray-300 hover:text-emerald-400 transition duration-300
+					 ease-in-ou"
+              >
+                Directory
+              </span>
+            </Link>
+
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
+            >
+              <span
+                className="text-gray-300 hover:text-emerald-400 transition duration-300
+					 ease-in-ou"
+              >
+                Themes
+              </span>
+            </Link>
+
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
+            >
+              <span
+                className="text-gray-300 hover:text-emerald-400 transition duration-300
+					 ease-in-ou"
+              >
+                Colors
               </span>
             </Link>
           </nav>
