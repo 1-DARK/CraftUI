ef198f7ac7cf708e73cca2a358f88e1365dcb7a4
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-02 13:32:07 +0530

    add navbar and homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-      <p className="bg-amber-200">hi</p>
+    <div className="min-h-screen relative overflow-hidden">
+      {/* Background gradient */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full" />
+        </div>
+      </div>
+
+      <div className="relative z-50 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path={"/"} element={<HomePage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
